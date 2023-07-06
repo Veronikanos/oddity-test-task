1e3b8abc5c98ce533dbdeadb4ec58366137337d4
Veronica <vveronikanika@gmail.com>
2023-07-06 14:34:19 +0300

    feat: add js, change logo by click to btn

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("test");
+window.addEventListener('DOMContentLoaded', () => {
+  const luckButton = document.querySelector('[data-change]');
+  const letters = document.querySelectorAll('.logo-letter');
+  const newLogo = 'Oddity';
+
+  luckButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    letters.forEach((letter, idx) => {
+      letter.textContent = newLogo[idx];
+    });
+  });
+});
